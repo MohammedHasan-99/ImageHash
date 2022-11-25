43038b373d43f553c2b2d5c368baef800c693ef2
Mohammed Hasan <79696955+MohammedHasan-99@users.noreply.github.com>
2022-11-25 23:52:23 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ secret_access_key = ""
 s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
 bucket = 'imagehashcloudproject'
 
-client = boto3.client('autoscaling', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+client = boto3.client('autoscaling', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key, region_name='us-east-1')
 
 
 
